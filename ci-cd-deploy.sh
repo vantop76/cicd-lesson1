7b4721935fa2aa592c8802aaea9e5bd68a727101
vano <hookcook@gmail.com>
2024-09-25 11:47:16 +0000

    First commit

---
+++ ci-cd-deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Пример деплоя ci-cd
+
+echo "<html><head></head><body>Hello World!</body></html>" > ./lesson-1/index.html
+echo ""
+echo ""
+echo ""
+### Добавить удаленный репозиторий
+# git remote add origin git@github.com:vantop76/cicd-lesson1.git
+# git branch -M main
+# git push -u origin main  # https://docs.github.com/get-started/getting-started-with-git/about-remote-repositories
+
+
